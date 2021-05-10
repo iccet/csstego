435e29edcb7096f12e21dd075c2b1d156b9e175b
kupns <kupns@mail.ru>
2021-05-10 19:22:29 +0300

    add bitmap decode impl

--- src/AbstractEncoder.cs
+++ src/AbstractEncoder.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace CsStg
@@ -32,6 +35,48 @@ namespace CsStg
             {
                 return Encode(data, p, container.Length);
             }
+        }
+        
+        public string Decode(Bitmap bitmap)
+        {
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            var bmpData = bitmap.LockBits(
+                rect, 
+                ImageLockMode.ReadWrite,
+                bitmap.PixelFormat);
+
+            var ptr = bmpData.Scan0;
+            var length = bmpData.Stride * bitmap.Height;
+            var bytes = new byte[length];
+            Marshal.Copy(ptr, bytes, 0, length);
+
+            var decoded = Decode(bytes);
+
+            bitmap.UnlockBits(bmpData);
+            return decoded;
+        }
+
+        public bool Encode(string data, Bitmap bitmap, MemoryStream stream)
+        {
+            var rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            var bmpData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                bitmap.PixelFormat);
+
+            var ptr = bmpData.Scan0;
+            var length = bmpData.Stride * bitmap.Height;
+            var bytes = new byte[length];
+            Marshal.Copy(ptr, bytes, 0, length);
+
+            var success = Encode(data, bytes);
+
+            Marshal.Copy(bytes, 0, ptr, length);
+            bitmap.UnlockBits(bmpData);
+            bitmap.Save(stream, bitmap.RawFormat);
+            stream.Position = 0;
+            
+            return success;
         }
 
     }
